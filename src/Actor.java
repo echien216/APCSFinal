9b5a333eee051792dfcd83d2a3d9deb1cd8bc28e
echien216 <echien216@LB19.hhs.win.fuhsd.org>
2017-05-09 14:09:30 -0700

    more methods, javadoc comments

--- src/Actor.java
+++ src/Actor.java
@@ -1,15 +1,52 @@
+import java.awt.Graphics;
 import java.awt.Rectangle;
 
-/** 
+/** An <code>Actor<\code> represents anything in
+ * Mater Tua that can move.
  * @author eugenia
  */
-
 
 public class Actor 
 {
 	private Rectangle hitbox;
+	private double vx, vy;
 	
-	public Actor(double x, double y)
+	public static final int WIDTH = 10;
+	
+	/**
+	 * Creates an Actor object. Its "hitbox" used for collision detection has
+	 * its top left corner at (x, y), and its width is 10 pixels.
+	 * @param x x coordinate of hitbox's top left corner
+	 * @param y y coordinate of hitbox's top left corner
+	 */
+	public Actor(int x, int y)
+	{
+		hitbox = new Rectangle(x, y, WIDTH, WIDTH);
+		vx = 0;
+		vy = 0;
+	}
+	
+	/**
+	 * Returns this Actor's hitbox.
+	 */
+	public Rectangle getHitbox()
+	{
+		return hitbox;
+	}
+	
+	/**
+	 * 
+	 */
+	public void move()
+	{
+		
+	}
+	
+	/**
+	 * Draws this Actor.
+	 * @param g the Graphics object used to draw the Actor. Must not be null. 
+	 */
+	public void draw(Graphics g)
 	{
 		
 	}
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,18 +1,44 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Rectangle;
 
-/**
- * 
+/** An <code>Obstacle<\code> represents anything in
+ * Mater Tua that <code>Actors<\code> cannot
+ * move through.
  * @author eugenia
- *
  */
 
 public class Obstacle 
 {
 	private Rectangle hitbox;
-	public static final int width = 10;
+	public static final int WIDTH = 10;
 	
+	/**
+	 * Creates an Obstacle object. Its "hitbox" used for collision detection has
+	 * its top left corner at (x, y), and its width is 10 pixels.
+	 * @param x x coordinate of hitbox's top left corner
+	 * @param y y coordinate of hitbox's top left corner
+	 */
 	public Obstacle(int x, int y)
 	{
-		hitbox = new Rectangle(x, y, width, width);
+		hitbox = new Rectangle(x, y, WIDTH, WIDTH);
+	}
+	
+	/**
+	 * Returns this Obstacle's hitbox.
+	 */
+	public Rectangle getHitbox()
+	{
+		return hitbox;
+	}
+	
+	/**
+	 * Draws this Obstacle.
+	 * @param g the Graphics object used to draw the Obstacle. Must not be null. 
+	 */
+	public void draw(Graphics g)
+	{
+		g.setColor(new Color(204, 128, 47));
+		g.drawRect(hitbox.x, hitbox.y, WIDTH, WIDTH);
 	}
 }
