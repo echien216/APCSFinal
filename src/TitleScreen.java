63aad13fbdd38ef52aea1a22fa0f4cfd955f5afb
Christine <Christine@172.20.178.171>
2017-05-16 17:17:45 -0700

    Eyy

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,19 +1,56 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 
-public class TitleScreen extends JPanel {
+public class TitleScreen extends JPanel implements ActionListener{
 
+	JButton play, instructions;
+	JPanel playPanel,instruPanel,blankPanel;
+	
 	public TitleScreen() {
 		super();
 		setSize(960,540);
+		
+		
+		setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
+		initButtons();
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.RED);
-		g.fillRect(100,100,960,540);
+		g.fillRect(0,0,960,540);
 		
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+	
+	private void initButtons(){
+		FlowLayout settings = new FlowLayout();
+		playPanel = new JPanel(settings);
+		instruPanel = new JPanel(settings);
+		blankPanel = new JPanel(settings);
+		
+		playPanel.setOpaque(false);
+		instruPanel.setOpaque(false);
+		blankPanel.setOpaque(false);
+		
+		play = new JButton("play");
+		instructions = new JButton("instructions");
+		play.addActionListener(this);
+		instructions.addActionListener(this);
+		
+		
+		playPanel.add(play);
+		instruPanel.add(instructions);
+		add(blankPanel);
+		add(playPanel);
+		add(instruPanel);
 	}
 }
